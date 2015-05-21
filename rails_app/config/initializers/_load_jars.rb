2a092f2dfc7ddb92451676bde638969d02250a81
wti <wti@g.xsidekick.com>
2015-05-21 17:41:37 +0700

    x

--- rails_app/config/initializers/_load_jars.rb
+++ rails_app/config/initializers/_load_jars.rb
@@ -1,21 +1,24 @@
-#require 'xls'
-require "pathname"
+if defined?(JRUBY_VERSION)
 
-def rec_path(path, file= false)
-  path.children.collect do |child|
-    if file and child.file?
-      child
-    elsif child.directory?
-      rec_path(child, file) + [child]
-    end
-  end.select { |x| x }.flatten(1)
-end
-puts "Load JARs"
-total = 0
-rec_path(Pathname.new(Rails.root.join "jars"), true).each{|entry|
-  if entry.to_s.end_with? ".jar"
-    total += 1
-    require entry.to_s
+  require "pathname"
+
+  def rec_path(path, file= false)
+    path.children.collect do |child|
+      if file and child.file?
+        child
+      elsif child.directory?
+        rec_path(child, file) + [child]
+      end
+    end.select { |x| x }.flatten(1)
   end
-}
-puts "Load JARs completed: total = #{total}"
+  puts "Load JARs"
+  total = 0
+  rec_path(Pathname.new(Rails.root.join "jars"), true).each{|entry|
+    if entry.to_s.end_with? ".jar"
+      total += 1
+      require entry.to_s
+    end
+  }
+  puts "Load JARs completed: total = #{total}"
+  
+end
