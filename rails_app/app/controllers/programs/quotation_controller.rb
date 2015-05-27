51af2bc70ae8bea35b9366b6df1d112f47332b83
wti <wti@g.xsidekick.com>
2015-05-27 19:18:36 +0700

    add list @first quotation page

--- rails_app/app/controllers/programs/quotation_controller.rb
+++ rails_app/app/controllers/programs/quotation_controller.rb
@@ -1,4 +1,80 @@
 class Programs::QuotationController < ResourceHelperController
+  def index_list result
+    qa = TbQuotation.arel_table
+    ct = RefCustomer.arel_table
+    ur = User.arel_table
+    ft = RefFreightTerm.arel_table
+    it = TbQuotationItem.arel_table
+    ut = RefUnitPrice.arel_table
+    pt = RefPartName.arel_table
+    md = RefModel.arel_table
+
+    projects = {
+      "record_id"     => qa[:id],
+      "uuid"          => qa[:uuid],
+      "quotation_no"  => qa[:quotation_no],
+      "customer"      => ct[:display_name],
+      "created_by"    => XModelUtils.desc(ur[:first_name], ur[:last_name], ' '),
+      "issue_date"    => qa[:issue_date],
+      "freight_term"  => ft[:display_name],
+      "exchange_rate" => qa[:exchange_rate],
+      "item_code"     => it[:item_code],
+      "sub_code"      => it[:sub_code],
+      "part_price"    => it[:part_price],
+      "po_reference"  => it[:po_reference],
+      "remark"        => it[:remark],
+      "package_price" => it[:package_price],
+      "customer_code" => it[:customer_code],
+      "unit_price"    => ut[:display_name],
+      "part_name"     => pt[:display_name],
+      "model"         => md[:display_name]
+    }
+
+    stmt = qa.project(project_stmt projects)
+      .join(ur).on(ur[:uuid].eq(qa[:created_by]))
+      .join(it).on(it[:quotation_uuid].eq(qa[:uuid]))
+
+
+    RefCustomer.join_me stmt, ct, qa
+    RefFreightTerm.join_me stmt, ft, qa
+
+    RefPartName.join_me stmt, pt, it
+    RefModel.join_me stmt, md, it
+    RefUnitPrice.join_me stmt, ut, it
+
+    #RefPartName.join_me stmt, pt, it
+    #RefModel.join_me stmt, md, it
+    #RefUnitPrice.join_me stmt, ut, it
+
+    result[:rows] = result_rows stmt 
+    result[:total] = result_total stmt
+
+  end
+
+  def create_quotation result
+    user = current_user
+
+    n = TbQuotation.new
+    n.uuid                  = params[:data][:uuid]
+    n.quotation_no          = params[:data][:quotation_no]
+    n.ref_customer_uuid     = params[:data][:customer]
+    n.issue_date            = params[:data][:issue_date]
+    n.ref_freight_term_uuid = params[:data][:freight_term]
+    n.exchange_rate         = params[:data][:exchange_rate]
+    n.created_by            = user.uuid 
+    n.updated_by            = user.uuid
+    n.save!
+
+  end
+
+  def destroy_approve_file result
+    TbQuotationApproveFile.where(id: params[:id]).delete_all
+  end
+
+  def destroy_calculation_file result
+    TbQuotationCalculationFile.where(id: params[:id]).delete_all
+  end
+
   def store_file n
     user = current_user
     n.tb_quotation_uuid = params[:uuid]
@@ -97,10 +173,6 @@ class Programs::QuotationController < ResourceHelperController
     RefPartName.join_me stmt, pt, it
     RefModel.join_me stmt, md, it
     RefUnitPrice.join_me stmt, ut, it
-
-    puts
-    puts stmt.to_sql
-    puts
 
     rows = TbQuotationItem.find_by_sql(stmt)
     result[:rows] = rows
