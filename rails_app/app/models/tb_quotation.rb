2bd05fa703ae65fb00a8f5f4efb0290842e5eefd
wti <wti@g.xsidekick.com>
2015-06-01 13:19:11 +0700

    d

--- rails_app/app/models/tb_quotation.rb
+++ rails_app/app/models/tb_quotation.rb
@@ -2,12 +2,10 @@ class TbQuotation < ActiveRecord::Base
   def download_details_excel
     uuid = UUID.generate
     Axlsx::Package.new do |p|
-      p.workbook.add_worksheet(:name => "Pie Chart") do |sheet|
-        sheet.add_row ["Simple Pie Chart"]
-        %w(first second third).each { |label| sheet.add_row [label, rand(24)+1] }
-        sheet.add_chart(Axlsx::Pie3DChart, :start_at => [0,5], :end_at => [10, 20], :title => "example 3: Pie Chart") do |chart|
-          chart.add_series :data => sheet["B2:B4"], :labels => sheet["A2:A4"],  :colors => ['FF0000', '00FF00', '0000FF']
-        end
+      p.workbook.add_worksheet(:name => "#{self.quotation_no}") do |sheet|
+        sheet.add_row ["First Column", "Second", "Third"]
+        #sheet.add_row [1, 2, 3]
+        
       end
       p.serialize(Rails.root.join 'tmp', uuid)
     end
